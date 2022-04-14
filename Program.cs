d520621a8ff903180e1878833c0a7b4caf2197ee
Murad Ragimov <ragimov73ya@mail.ru>
2022-04-14 13:01:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Zadanie_XX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] res = new int[array.Length];
+            for (int i = 0; i < res.Length; i++)
+            {
+                res[i] = array[i]*2;
+            }
+            Printer(res);
+        }
+        static void Printer(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
